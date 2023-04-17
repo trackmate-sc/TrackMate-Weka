b838aa570489008f160ab5f2ca0c2e5e9fe7966e
Jean-Yves Tinevez <tinevez@pasteur.fr>
2023-04-17 20:44:20 +0200

    Update interactive test.

--- src/test/java/fiji/plugin/trackmate/weka/TrackMateWeka3DTestDrive.java
+++ src/test/java/fiji/plugin/trackmate/weka/TrackMateWeka3DTestDrive.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -37,7 +37,8 @@ public class TrackMateWeka3DTestDrive
 	{
 		UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
 		ImageJ.main( args );
-		final String path = "samples/merged.tif";
+//		final String path = "samples/merged.tif";
+		final String path = "samples/mesh/CElegansMask3D.tif";
 		IJ.openImage( path ).show();
 		new TrackMatePlugIn().run( null );
 	}
