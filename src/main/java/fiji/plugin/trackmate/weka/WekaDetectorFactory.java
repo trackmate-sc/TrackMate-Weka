371e11060e0a7162eb3993821d5a7c93b5d3fe68
Jean-Yves Tinevez <tinevez@pasteur.fr>
2025-07-18 16:07:03 +0200

    Adapt to new TrackMate v8 API

--- src/main/java/fiji/plugin/trackmate/weka/WekaDetectorFactory.java
+++ src/main/java/fiji/plugin/trackmate/weka/WekaDetectorFactory.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -23,22 +23,10 @@ package fiji.plugin.trackmate.weka;
 
 import static fiji.plugin.trackmate.detection.DetectorKeys.DEFAULT_TARGET_CHANNEL;
 import static fiji.plugin.trackmate.detection.DetectorKeys.KEY_TARGET_CHANNEL;
-import static fiji.plugin.trackmate.io.IOUtils.readDoubleAttribute;
-import static fiji.plugin.trackmate.io.IOUtils.readIntegerAttribute;
-import static fiji.plugin.trackmate.io.IOUtils.readStringAttribute;
-import static fiji.plugin.trackmate.io.IOUtils.writeAttribute;
-import static fiji.plugin.trackmate.io.IOUtils.writeTargetChannel;
-import static fiji.plugin.trackmate.util.TMUtils.checkMapKeys;
-import static fiji.plugin.trackmate.util.TMUtils.checkParameter;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import javax.swing.ImageIcon;
-
-import org.jdom2.Element;
 import org.scijava.plugin.Plugin;
 
 import fiji.plugin.trackmate.Model;
@@ -61,9 +49,8 @@ public class WekaDetectorFactory< T extends RealType< T > & NativeType< T > > im
 	/*
 	 * CONSTANTS
 	 */
-	/**
-	 * The key to the parameter that stores the path to the Weka classifier.
-	 */
+
+	/** The key to the parameter that stores the path to the Weka classifier. */
 	public static final String KEY_CLASSIFIER_FILEPATH = "CLASSIFIER_FILEPATH";
 
 	/**
@@ -88,6 +75,8 @@ public class WekaDetectorFactory< T extends RealType< T > & NativeType< T > > im
 	/** The pretty name of the target detector. */
 	public static final String NAME = "Weka detector";
 
+	public static final String DOC_URL = "https://imagej.net/plugins/trackmate/detectors/trackmate-weka";
+
 	/** An html information text. */
 	public static final String INFO_TEXT = "<html>"
 			+ "This detector relies on the 'Trainable Weka segmentation' plugin to detect objects."
@@ -108,30 +97,45 @@ public class WekaDetectorFactory< T extends RealType< T > & NativeType< T > > im
 			+ "</html>";
 
 	/*
-	 * FIELDS
-	 */
-
-	/** The image to operate on. Multiple frames, single channel. */
-	protected ImgPlus< T > img;
-
-	protected Map< String, Object > settings;
-
-	protected String errorMessage;
-
-	protected WekaRunner< T > runner;
-
-	/*
 	 * METHODS
 	 */
 
 	@Override
-	public SpotDetector< T > getDetector( final Interval interval, final int frame )
+	public String checkSettings( final Map< String, Object > settings )
+	{
+		final String error = SpotDetectorFactory.super.checkSettings( settings );
+		if ( error != null )
+			return error;
+
+		// First test to make sure we can read the classifier file.
+		final Object obj = settings.get( KEY_CLASSIFIER_FILEPATH );
+		if ( obj == null )
+			return "The path to the Weka classifier file is not set.";
+
+		final StringBuilder errorHolder = new StringBuilder();
+		if ( !IOUtils.canReadFile( ( String ) obj, errorHolder ) )
+			return "Problem with Weka classifier file: " + errorHolder.toString();
+
+		return null;
+	}
+
+	@Override
+	public SpotDetector< T > getDetector( final ImgPlus< T > img, final Map< String, Object > settings, final Interval interval, final int frame )
 	{
 		final int channel = ( Integer ) settings.get( KEY_TARGET_CHANNEL ) - 1;
 		final ImgPlus< T > input = TMUtils.hyperSlice( img, channel, frame );
 		final int classIndex = ( Integer ) settings.get( KEY_CLASS_INDEX );
 		final double probaThreshold = ( Double ) settings.get( KEY_PROBA_THRESHOLD );
 		final boolean simplify = true;
+		final boolean is3D = img.dimensionIndex( Axes.Z ) >= 0;
+		final WekaRunner< T > runner = new WekaRunner<>( settings.get( KEY_CLASSIFIER_FILEPATH ).toString(), is3D );
+		if ( !runner.loadClassifier() )
+		{
+			final String errorMessage = runner.getErrorMessage();
+			System.err.println( errorMessage );
+			return null;
+		}
+
 		final WekaDetector< T > detector = new WekaDetector<>(
 				runner,
 				input,
@@ -153,74 +157,9 @@ public class WekaDetectorFactory< T extends RealType< T > & NativeType< T > > im
 	}
 
 	@Override
-	public boolean setTarget( final ImgPlus< T > img, final Map< String, Object > settings )
+	public boolean has2Dsegmentation()
 	{
-		// First test to make sure we can read the classifier file.
-		final Object obj = settings.get( KEY_CLASSIFIER_FILEPATH );
-		if ( obj == null )
-		{
-			errorMessage = "The path to the Weka classifier file is not set.";
-			return false;
-		}
-
-		final StringBuilder errorHolder = new StringBuilder();
-		if ( !IOUtils.canReadFile( ( String ) obj, errorHolder ) )
-		{
-			errorMessage = "Problem with Weka classifier file: " + errorHolder.toString();
-			return false;
-		}
-
-		final String classifierFilePath = ( String ) obj;
-		final boolean is3D = img.dimensionIndex( Axes.Z ) >= 0;
-		this.runner = new WekaRunner<>( classifierFilePath, is3D );
-		if ( !runner.loadClassifier() )
-		{
-			errorMessage = runner.getErrorMessage();
-			return false;
-		}
-		this.img = img;
-		this.settings = settings;
-		return checkSettings( settings );
-	}
-
-	@Override
-	public String getErrorMessage()
-	{
-		return errorMessage;
-	}
-
-	@Override
-	public boolean marshall( final Map< String, Object > settings, final Element element )
-	{
-		final StringBuilder errorHolder = new StringBuilder();
-		boolean ok = writeTargetChannel( settings, element, errorHolder );
-		ok = ok && writeAttribute( settings, element, KEY_CLASSIFIER_FILEPATH, String.class, errorHolder );
-		ok = ok && writeAttribute( settings, element, KEY_CLASS_INDEX, Integer.class, errorHolder );
-		ok = ok && writeAttribute( settings, element, KEY_PROBA_THRESHOLD, Double.class, errorHolder );
-
-		if ( !ok )
-			errorMessage = errorHolder.toString();
-
-		return ok;
-	}
-
-	@Override
-	public boolean unmarshall( final Element element, final Map< String, Object > settings )
-	{
-		settings.clear();
-		final StringBuilder errorHolder = new StringBuilder();
-		boolean ok = true;
-		ok = ok && readIntegerAttribute( element, settings, KEY_TARGET_CHANNEL, errorHolder );
-		ok = ok && readStringAttribute( element, settings, KEY_CLASSIFIER_FILEPATH, errorHolder );
-		ok = ok && readIntegerAttribute( element, settings, KEY_CLASS_INDEX, errorHolder );
-		ok = ok && readDoubleAttribute( element, settings, KEY_PROBA_THRESHOLD, errorHolder );
-
-		if ( !ok )
-		{
-			errorMessage = errorHolder.toString();
-			return false;
-		}
-		return checkSettings( settings );
+		return true;
 	}
 
 	@Override
@@ -236,58 +175,14 @@ public class WekaDetectorFactory< T extends RealType< T > & NativeType< T > > im
 		settings.put( KEY_TARGET_CHANNEL, DEFAULT_TARGET_CHANNEL );
 		settings.put( KEY_CLASS_INDEX, DEFAULT_CLASS_INDEX );
 		settings.put( KEY_PROBA_THRESHOLD, DEFAULT_PROBA_THRESHOLD );
-		settings.put( KEY_CLASSIFIER_FILEPATH, null );
+		settings.put( KEY_CLASSIFIER_FILEPATH, "" );
 		return settings;
-	}
-
-	@Override
-	public boolean checkSettings( final Map< String, Object > settings )
-	{
-		boolean ok = true;
-		final StringBuilder errorHolder = new StringBuilder();
-		ok = ok & checkParameter( settings, KEY_TARGET_CHANNEL, Integer.class, errorHolder );
-		ok = ok & checkParameter( settings, KEY_CLASS_INDEX, Integer.class, errorHolder );
-		ok = ok & checkParameter( settings, KEY_PROBA_THRESHOLD, Double.class, errorHolder );
-		ok = ok & checkParameter( settings, KEY_CLASSIFIER_FILEPATH, String.class, errorHolder );
-		final List< String > mandatoryKeys = new ArrayList<>();
-		mandatoryKeys.add( KEY_TARGET_CHANNEL );
-		mandatoryKeys.add( KEY_CLASS_INDEX );
-		mandatoryKeys.add( KEY_PROBA_THRESHOLD );
-		mandatoryKeys.add( KEY_CLASSIFIER_FILEPATH );
-		ok = ok & checkMapKeys( settings, mandatoryKeys, null, errorHolder );
-		if ( !ok )
-			errorMessage = errorHolder.toString();
-
-		// Extra test to make sure we can read the classifier file.
-		if ( ok )
-		{
-			final Object obj = settings.get( KEY_CLASSIFIER_FILEPATH );
-			if ( obj == null )
-			{
-				errorMessage = "The path to the Weka classifier file is not set.";
-				return false;
-			}
-
-			if ( !IOUtils.canReadFile( ( String ) obj, errorHolder ) )
-			{
-				errorMessage = "Problem with Weka classifier file: " + errorHolder.toString();
-				return false;
-			}
-		}
-
-		return ok;
 	}
 
 	@Override
 	public String getInfoText()
 	{
 		return INFO_TEXT;
-	}
-
-	@Override
-	public ImageIcon getIcon()
-	{
-		return null;
 	}
 
 	@Override
@@ -303,14 +198,8 @@ public class WekaDetectorFactory< T extends RealType< T > & NativeType< T > > im
 	}
 
 	@Override
-	public boolean has2Dsegmentation()
+	public String getUrl()
 	{
-		return true;
-	}
-
-	@Override
-	public WekaDetectorFactory< T > copy()
-	{
-		return new WekaDetectorFactory<>();
+		return DOC_URL;
 	}
 }
